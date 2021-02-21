ec2a0164472305bd37a86264ebc5e344f84a1e26
Yslan Ramos <yslan3000@gmail.com>
2021-02-21 18:09:39 -0300

    add tasks in local storage

--- src/store/TodoContext.js
+++ src/store/TodoContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useCallback, useEffect, useState } from 'react'
 import { generateID } from '../utils/generateID'
 
 const INITIAL_VALUE = { tasks: [] }
@@ -12,6 +12,22 @@ export const TodoProvider = ({ children }) => {
     setTasks([...tasks, { id: generateID(), content }])
   const remTask = (taskID) =>
     setTasks(tasks.filter((task) => task.id !== taskID))
+
+  useEffect(() => {
+    const storageTasks = JSON.parse(localStorage.getItem('tasks'))
+    if (Array.isArray(storageTasks)) {
+      setTasks(storageTasks)
+    }
+  }, [])
+
+  const setTasksInLocalStorage = useCallback(() => {
+    const tasksJSON = JSON.stringify(tasks)
+    localStorage.setItem('tasks', tasksJSON)
+  }, [tasks])
+
+  useEffect(() => {
+    setTasksInLocalStorage()
+  }, [setTasksInLocalStorage])
 
   return (
     <TodoContext.Provider value={{ tasks, addTask, remTask }}>
