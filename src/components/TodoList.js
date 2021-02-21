e98548d498295262372ec52cc291b370ec86edcb
Yslan Ramos <yslan3000@gmail.com>
2021-02-21 16:30:22 -0300

    move trash icon to start and add hover

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,13 +2,18 @@ import React from 'react'
 import { ListGroup } from 'react-bootstrap'
 import { AiFillDelete } from 'react-icons/ai'
 
+import './TodoList.css'
+
 const TodoList = ({ tasks, onDelete }) => {
   return (
     <ListGroup>
       {tasks.map((task) => (
         <ListGroup.Item key={task.id}>
+          <AiFillDelete
+            className='delete-button'
+            onClick={(event) => onDelete(task.id, event)}
+          />{' '}
           {task.content}
-          <AiFillDelete onClick={(event) => onDelete(task.id, event)} />
         </ListGroup.Item>
       ))}
     </ListGroup>
